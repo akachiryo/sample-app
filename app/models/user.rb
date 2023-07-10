414c2b41a1fc14f3909377b84787618491f0380d
Kazuki Mamiya <ubuntu@ip-172-31-22-13.ap-northeast-1.compute.internal>
2023-07-10 08:16:47 +0000

    Add address format validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
   #存在性の検証
-  validates :name, presence: true
-  #validates(:name, presence: true)と同等
+  validates :name, presence: true #validates(:name, presence: true)と同等
   validates :email, presence: true
 
   #長さの検証
   validates :name,  presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i    #フォーマットの検証
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },      #フォーマットの検証
+                    uniqueness: { case_sensitive: false}      #一意性の検証
+
 end
