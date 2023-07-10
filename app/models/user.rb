2bad4592742d7079ef7d2fdb9acad3f1975230fb
Kazuki Mamiya <ubuntu@ip-172-31-22-13.ap-northeast-1.compute.internal>
2023-07-10 04:58:44 +0000

    Add User lebgth  validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
+  #存在性の検証
   validates :name, presence: true
   #validates(:name, presence: true)と同等
   validates :email, presence: true
+
+  #長さの検証
+  validates :name,  presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }
 end
