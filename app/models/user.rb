a193f0ce80977ee400980ccb966b23fd855ae468
Kazuki Mamiya <ubuntu@ip-172-31-22-13.ap-northeast-1.compute.internal>
2023-07-11 01:26:09 +0000

    Add address uniqueness validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,10 @@ class User < ApplicationRecord
   validates :name, presence: true #validates(:name, presence: true)と同等
   validates :email, presence: true
 
-  #長さの検証
-  validates :name,  presence: true, length: { maximum: 50 }
+  before_save { self.email = email.downcase }                 #保存前に全て小文字にする
+  validates :name,  presence: true, length: { maximum: 50 }   #長さの検証
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i    #フォーマットの検証
-  validates :email, presence: true, length: { maximum: 255 },
+  validates :email, presence: true, length: { maximum: 255 }, #長さの検証
                     format: { with: VALID_EMAIL_REGEX },      #フォーマットの検証
-                    uniqueness: { case_sensitive: false}      #一意性の検証
-
+                    uniqueness: true                          #一意性の検証
 end
